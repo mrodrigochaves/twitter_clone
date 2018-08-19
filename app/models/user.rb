376799eac2b5d454a22812aaf89c8b754eeb388c
Marcio Rodrigo <marciorodrigosp@gmail.com>
2018-08-19 13:15:34 -0300

    Atualizando Relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
+  has_many :active_relationships, class_name:'Relationship',
+  			foreign_key: 'follower_id', dependent:   :destroy
+
+  has_many :passive_relationships, class_name:'Relationship',
+  			foreign_key: 'followed_id', dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :followers, through: :active_relationships, source: :follower
+
+  def following?(other_user)
+  	# Este método criará o relacionamento entre um usuário e outro.
+  end
+
+  def follow!(other_user)
+  	# Este método continuara o relacionamento entre um usuário e outro.
+  end
+
+  def unfollow!(other_user)
+  	# Este método apagará o relacionamento entre um usuário e outro.
+  end
+
 end
